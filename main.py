8b4a55dd12430c142ca233c160f0506084c7c17d
Lucas <lucas.humbert.28@gmail.com>
2023-12-12 17:52:20 +0100

    fix refacto

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def main():
     firstColor = input('Couleur en commun ? ').split(',')
     secondColor = input('Couleur secrète ? ').split(',')
 
-    printColors(crypt(firstColor, secondColor))
+    printColors(firstMix(firstColor, secondColor))
 
     transportedColor = input('Couleur de votre correspondant ? ').split(',')
 
-    printColors(decrypt(firstColor, transportedColor))
+    printColors(secondMix(firstColor, transportedColor))
 
 
-def crypt(common_color, secret_color):
+def firstMix(common_color, secret_color):
     red = (int(common_color[0]) + int(secret_color[0])) / 2
     green = (int(common_color[1]) + int(secret_color[1])) / 2
     blue = (int(common_color[2]) + int(secret_color[2])) / 2
@@ -24,7 +24,7 @@ def crypt(common_color, secret_color):
     return [round(red), round(green), round(blue)]
 
 
-def decrypt(transported_color, secret_color):
+def secondMix(transported_color, secret_color):
     red = (int(transported_color[0]) * 2 + int(secret_color[0])) / 3
     green = (int(transported_color[1]) * 2 + int(secret_color[1])) / 3
     blue = (int(transported_color[2]) * 2 + int(secret_color[2])) / 3
