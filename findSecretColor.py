b23da0bdff2d96007171ce116c64cb5da4d2394c
BaptisteHouques <baptistehouques38@gmail.com>
2023-12-13 17:00:35 +0100

    fix

--- findSecretColor.py
+++ findSecretColor.py
@@ -1,9 +1,12 @@
+def parse_input(input_str):
+    return [int(x) for x in input_str.split(',')]
+
 def findSecretColor(pub_alice_color, pub_bob_color, common_color):
     red = pub_bob_color[0] * 2 - common_color[0]
     green = pub_bob_color[1] * 2 - common_color[1]
     blue = pub_bob_color[2] * 2 - common_color[2]
 
-    bob_secret = [red, green, blue]
+    bob_secret = [round(red), round(green), round(blue)]
 
     common_secret = secondMix(pub_alice_color, bob_secret)
 
@@ -16,10 +19,9 @@ def secondMix(transported_color, secret_color):
 
     return [round(red), round(green), round(blue)]
 
-
-base = input('Couleur en commun ? ').split(',')
-pubAlice = input('Couleur public Alice ? ').split(',')
-pubBob = input('Couleur public Bob ? ').split(',')
+base = parse_input(input('Couleur en commun ? '))
+pubAlice = parse_input(input('Couleur public Alice ? '))
+pubBob = parse_input(input('Couleur public Bob ? '))
 
 # Calcul du secret partagé (marron caca)
 print(findSecretColor(pubAlice, pubBob, base))
